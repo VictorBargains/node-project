3ab053fdcb293fee73ca5c99a4c30861a39f00f9
Jeff Mignone <jeff.mignone@gmail.com>
2018-06-03 15:44:02 -0400

    added userRouter

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import userData from './user-data.json';
 
+import userRouter from './userRouter';
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -9,10 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 app.use(express.static('./public'));
+app.use('/api/user', userRouter);
 
-app.get('/api/user', (req, res) => {  
-    res.json(userData);
-});
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
