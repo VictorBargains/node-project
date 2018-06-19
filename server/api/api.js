878784729cf4dfd9414b9ec926adcb0599cd083b
Gabriel Ferraz <gabrielferraz@fullsail.edu>
2018-06-18 20:51:47 -0400

    Update api.js

--- server/api/api.js
+++ server/api/api.js
@@ -1,8 +1,6 @@
 
 import user from './user/userRoutes.js';
-// import animal from './user/animalRoutes.js';
 
 export default app => {
   app.use('/api/user', user);
-  // app.use('/api/animal', animal);
 };
