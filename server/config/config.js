d809a99fbaebbb85d287538d72540ffcc20735ff
Gabriel Ferraz <gabrielferraz@fullsail.edu>
2018-06-20 20:58:03 -0400

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,13 @@
 const config = {
     PORT: process.env.PORT || 8080,
-    ENV: process.env.NODE_ENV || 'dev'
+    ENV: process.env.NODE_ENV || 'dev',
+    // DATABASE_URL: getDBUrl();
 }
 
+// "dev" or "development"? Stick with one
+
+// you could create a function that returns DB URL and invoke that function in the config obj itself
+// design a function that doesn't use "if statements"; think of the data structures the language offers you
 if (config.ENV === 'development' || config.ENV === 'production') {
     config.DATABASE_URL = process.env.DATABASE_URL;
 } else if (config.ENV === 'test') {
