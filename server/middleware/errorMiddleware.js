6aefdea6f04707f0b74e49cd9a07bb3d0b558cfe
Gabriel Ferraz <gabrielferraz@fullsail.edu>
2018-06-20 20:53:59 -0400

    Update errorMiddleware.js

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,8 +1,11 @@
+// WHY DO YOU NEED THESE IMPORTS?
 import { ApiException } from '../errorHandlers/exceptions';
 import { errorHandler } from '../api/apiHelpers';
 
 export default (app) => {
     app.use((err, req, res, next) => {
+        // YOU NEED TO PLAN HOW YOUR ERRORS WILL BE PASSED DOWN TO THIS MIDDLEWARE
+        // OR HANDLE DIFFERENT ERROR TYPES (OBJECTS OR STRINGS) HERE IN THIS FILE
         res.json(err);
     });
 }
