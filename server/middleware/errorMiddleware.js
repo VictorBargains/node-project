e80f3f316c49a5905e6c5e7f0e6ae7c7ccfeb18a
Jeff Mignone <jeff.mignone@gmail.com>
2018-06-21 19:38:17 -0400

    removed unneeded imports

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,7 +1,3 @@
-// WHY DO YOU NEED THESE IMPORTS?
-import { ApiException } from '../errorHandlers/exceptions';
-import { errorHandler } from '../api/apiHelpers';
-
 export default (app) => {
     app.use((err, req, res, next) => {
         // YOU NEED TO PLAN HOW YOUR ERRORS WILL BE PASSED DOWN TO THIS MIDDLEWARE
