d6b9121e83639b5b4d7d6917aec3ef548ce4162b
Gabriel Ferraz <gabrielferraz@fullsail.edu>
2018-06-20 21:07:09 -0400

    Update userCtrl.js

--- server/api/user/userCtrl.js
+++ server/api/user/userCtrl.js
@@ -3,41 +3,49 @@ import { validateFields, errorHandler } from '../apiHelpers';
 import { ApiException } from '../../errorHandlers/exceptions';
 
 export default {
-	getUsers(req, res, next) {  
-    User.find().then(docs => {
-      if (!docs.length) {
-        return errorHandler({
-          message: 'No resource found',
-          status: 404
-        }, ApiException, next);
-      }
-      res.json(docs);
-    })
-    .catch(err => {
-      return errorHandler(err, ApiException, next);
-    });
+  getUsers(req, res, next) {  
+    User.find()
+	 // CHOOSE SEMANTIC VARIABLE NAMES
+	 // WHEN YOU HAVE A LOT OF STUFF GOING ON
+	 // STACK THE CHAINED METHODS FOR BETTER READABILITY
+	.then(docs => {
+      	  if (!docs.length) {
+            	return errorHandler({
+              		message: 'No resource found',
+              		status: 404
+            }, ApiException, next);
+      	}
+	    
+      	res.json(docs);
+    	})
+    	.catch(err => {
+      		// DID YOU CHECK WHAT THIS ERROR LOOKS LIKE?
+      		return errorHandler(err, ApiException, next);
+    	});
   },
   getUser(req, res, next) {
-    User.findById(req.params.id).then(doc => {
-      res.json(doc);
-    })
-    .catch(err => {
+    User.findById(req.params.id)
+	.then(doc => {
+      	   	res.json(doc);
+    	})
+    	.catch(err => {
       return errorHandler(err, ApiException, next);
     });
   },
   createUser(req, res, next) {
-
       validateFields(req, res, next)
       
       const newUser = new User(req.body);
 
       newUser.save()
-      .then(doc => {
-        res.json(doc);
-      })
-      .catch(err => {
-        return errorHandler(err, ApiException, next);
-      })
+      	.then(doc => {
+	      	// MAYBE "USER"?
+	      	// WHAT IF THERE IS NONE?
+        	res.json(doc);
+      	})
+      	.catch(err => {
+        	return errorHandler(err, ApiException, next);
+      	})
 
   },
   updateUser(req, res, next) {
