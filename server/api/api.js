8d4bb3457155ae318ee463a765348e4654fecfba
Jeff Mignone <jeff.mignone@gmail.com>
2018-06-23 18:08:21 -0400

    added recipe router

--- server/api/api.js
+++ server/api/api.js
@@ -1,6 +1,8 @@
-
 import user from './user/userRoutes.js';
+import recipe from './recipe/recipeRoutes.js';
+
 
 export default app => {
   app.use('/api/user', user);
+  app.use('/api/recipe', recipe);
 };
