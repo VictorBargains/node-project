a3eb12ef2cfd30b47e7082874a3944374ebc15d2
Jeff Mignone <jeff.mignone@gmail.com>
2018-06-23 18:09:15 -0400

    added error handling to userCtrl

--- server/api/user/userCtrl.js
+++ server/api/user/userCtrl.js
@@ -1,14 +1,11 @@
 import User from './userModel';
-import { validateFields, errorHandler } from '../apiHelpers';
+import { validateFields, errorHandler, checkIdsMatch, checkIfEmpty } from '../apiHelpers';
 import { ApiException } from '../../errorHandlers/exceptions';
 
 export default {
   getUsers(req, res, next) {  
     User.find()
-	 // CHOOSE SEMANTIC VARIABLE NAMES
-	 // WHEN YOU HAVE A LOT OF STUFF GOING ON
-	 // STACK THE CHAINED METHODS FOR BETTER READABILITY
-	.then(users => {
+		.then(users => {
       	  if (!users.length) {
             	return errorHandler({
               		message: 'No resource found',
@@ -25,33 +22,72 @@ export default {
   },
   getUser(req, res, next) {
     User.findById(req.params.id)
-	.then(doc => {
-      	   	res.json(doc);
+		.then(user => {
+
+				if (!user) {
+						return errorHandler({
+							message: 'No resource found',
+							status: 404
+						}, ApiException, next);
+				}
+
+      	  res.json(user);
     	})
     	.catch(err => {
       return errorHandler(err, ApiException, next);
     });
   },
   createUser(req, res, next) {
-      validateFields(req, res, next)
       
       const newUser = new User(req.body);
 
-      newUser.save()
-      	.then(user => {
-	      	// MAYBE "USER"?
+      newUser.save(req.body)
+      .then(user => {
 	      	// WHAT IF THERE IS NONE?
         	res.json(user);
-      	})
-      	.catch(err => {
+      })
+      .catch(err => {
         	return errorHandler(err, ApiException, next);
-      	})
+      })
 
   },
   updateUser(req, res, next) {
 
+		checkIdsMatch(req, res, next);
+
+		checkIfEmpty(req, res, next);
+
+		User.findByIdAndUpdate(req.params.id, { $set: req.body }, { runValidators: true, new: true })
+		.then(updatedUser => {
+
+				if (!updatedUser) {
+					return errorHandler({
+						message: 'No resource found',
+						status: 404
+					}, ApiException, next);
+				}
+
+				res.json(updatedUser);
+		})
+		.catch(err => {
+			return errorHandler(err, ApiException, next);
+		});
   },
   deleteUser(req, res, next) {
+		User.findByIdAndRemove(req.params.id)
+		.then(deletedUser => {
 
+			if (!deletedUser) {
+				return errorHandler({
+					message: 'No resource found',
+					status: 404
+				}, ApiException, next);
+			}
+
+			res.json(deletedUser);
+		})
+		.catch(err => {
+			return errorHandler(err, ApiException, next);
+		}); 
   }
 };
