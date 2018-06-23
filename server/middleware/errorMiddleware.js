37ecbd55e86c5c059d5de5ce795f1e6ba39f2f34
Jeff Mignone <jeff.mignone@gmail.com>
2018-06-23 18:10:23 -0400

    added handling of obj or string

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -2,6 +2,13 @@ export default (app) => {
     app.use((err, req, res, next) => {
         // YOU NEED TO PLAN HOW YOUR ERRORS WILL BE PASSED DOWN TO THIS MIDDLEWARE
         // OR HANDLE DIFFERENT ERROR TYPES (OBJECTS OR STRINGS) HERE IN THIS FILE
-        res.json(err);
+        
+        if (!err.status || err === String) {
+            err.message = err.message || err;
+            err.status = 500;
+        }
+
+        res.status(err.status).json(err);
+
     });
 }
