44009a6971e194614c63ee9f24f6dc16cdd895ad
Jeff Mignone <jeff.mignone@gmail.com>
2018-06-23 18:10:05 -0400

    revised config

--- server/config/config.js
+++ server/config/config.js
@@ -1,20 +1,19 @@
 const config = {
     PORT: process.env.PORT || 8080,
     ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: getDBUrl()
 };
 
-const getDBUrl = () => {
+const getDBUrl = (x) => {
     let envs = {
        development: 'mongodb://localhost/coffee-app-dev',
        production: process.env.DATABASE_URL,
        testing: 'mongodb://localhost/coffee-app-testing'
     };
     
-    return envs[config.ENV];
+    return envs[x.ENV];
 };
 
-// config.DATABASE_URL = getDBUrl(config.ENV);
+config.DATABASE_URL = getDBUrl(config);
 
 export default config;
 
