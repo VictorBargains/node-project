db626748b35a9474f64f5984cd23d638c80284b5
Jeff Mignone <jeff.mignone@gmail.com>
2018-06-24 21:42:36 -0400

    added ValidationException

--- server/errorHandlers/exceptions.js
+++ server/errorHandlers/exceptions.js
@@ -13,6 +13,15 @@ class ApiException extends Exception {
     }
 }
 
+class ValidationException extends Exception {
+    constructor(message, status, allErrors) {
+        super(message, status);
+        this.name = 'ValidationException';
+        this.errors = allErrors;
+    }
+}
+
 export {
-    ApiException
+    ApiException,
+    ValidationException
 }
