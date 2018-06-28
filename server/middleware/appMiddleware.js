39d04ad49ac83f706e9af84614b487e34b1ed78d
Jeff Mignone <jeff.mignone@gmail.com>
2018-06-28 00:28:04 -0400

    clean up and add session

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -1,8 +1,17 @@
 import morgan from 'morgan';
+import session from 'express-session';
+import passport from 'passport';
 
 export default (app, express) => {
 	app.use(express.json());
 	app.use(express.urlencoded({extended: true}));
+	app.use(session({ 
+		secret: 'jwm0019',
+		resave: false,
+		saveUninitialized: false
+	 }));
+	app.use(passport.initialize());
+	app.use(passport.session());
 	app.use(morgan('dev'));
 	app.use(express.static('./public'));
 };
--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -3,11 +3,10 @@ export default (app) => {
     app.use((err, req, res, next) => {
         // YOU NEED TO PLAN HOW YOUR ERRORS WILL BE PASSED DOWN TO THIS MIDDLEWARE
         // OR HANDLE DIFFERENT ERROR TYPES (OBJECTS OR STRINGS) HERE IN THIS FILE
-        
-        if (!err.status || err === String) {
-            err.message = err.message || err;
-            err.status = 500;
-        }
+
+        err === String ? err.message = err : err.message;
+        !err.status ? err.status = 500 : err.status; 
+
         next(err);
 
     });
